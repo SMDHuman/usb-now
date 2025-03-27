7db4bafb4a8a4879f8eb3d6dbec08cbf3ef0f341
SMDHuman <48795375+SMDHuman@users.noreply.github.com>
2025-03-27 13:21:03 +0300

    Refactor SLIP protocol handling and improve checksum calculation

---
+++ src/p_slip.h
@@ -0,0 +1,124 @@
+#ifndef SLIP_H
+#define SLIP_H
+
+//-----------------------------------------------------------------------------
+#include <cstdint>
+#include <cstddef>
+#include <string.h>
+
+//-----------------------------------------------------------------------------
+#define S_END 0xC0
+#define S_ESC 0xDB
+#define S_ESC_END 0xDC
+#define S_ESC_ESC 0xDD
+
+//-----------------------------------------------------------------------------
+struct slip_buffer_header_t
+{   
+    uint32_t len;
+    uint32_t size;
+    uint32_t checksum;
+    uint8_t ready;
+    uint8_t esc_flag;
+    uint8_t checksum_enable;
+    uint8_t overflow;
+};
+
+//-----------------------------------------------------------------------------
+void slip_init(uint8_t *buffer, uint32_t size, uint8_t checksum_enable = true);
+void slip_push(uint8_t *buffer, uint8_t data);
+uint32_t slip_get_size(uint8_t *buffer);
+uint8_t *slip_get_buffer(uint8_t *buffer);
+void slip_reset(uint8_t *buffer);
+uint8_t slip_is_ready(uint8_t *buffer);
+
+#endif
+
+//-----------------------------------------------------------------------------
+#ifdef SLIP_IMPLEMENTATION
+    //-----------------------------------------------------------------------------
+    void slip_init(uint8_t *buffer, uint32_t size, uint8_t checksum_enable){
+        slip_buffer_header_t slip_buffer_header;
+        slip_buffer_header.len = 0;
+        slip_buffer_header.size = size;
+        slip_buffer_header.checksum = 0;
+        slip_buffer_header.ready = false;
+        slip_buffer_header.esc_flag = false;
+        slip_buffer_header.checksum_enable = checksum_enable;
+        memcpy(buffer, &slip_buffer_header, sizeof(slip_buffer_header_t));   
+    }
+
+    //-----------------------------------------------------------------------------
+    void slip_push(uint8_t *buffer, uint8_t data){
+        slip_buffer_header_t *slip_buffer_header = (slip_buffer_header_t *)buffer;
+        uint8_t *data_buffer = buffer + sizeof(slip_buffer_header_t);
+        if(slip_buffer_header->size <= slip_buffer_header->len){
+            slip_reset(buffer);
+            slip_buffer_header->overflow = true;
+        }
+        if(slip_buffer_header->ready){
+            slip_reset(buffer);
+        }
+        if(slip_buffer_header->esc_flag){
+            if(data == S_ESC_END){
+                data_buffer[slip_buffer_header->len++] = S_END;
+                slip_buffer_header->checksum += S_END+1;
+            }
+            else if(data == S_ESC_ESC){
+                data_buffer[slip_buffer_header->len++] = S_ESC;
+                slip_buffer_header->checksum += S_ESC+1;
+            }
+            slip_buffer_header->esc_flag = false;
+        }
+        else if(data == S_ESC){
+            slip_buffer_header->esc_flag = true;
+        }
+        else if(data == S_END){
+            slip_buffer_header->ready = true;
+            if(slip_buffer_header->checksum_enable == true){
+                slip_buffer_header->len -= 4;
+            }
+        }
+        else{
+            data_buffer[slip_buffer_header->len++] = data;
+            slip_buffer_header->checksum += data+1;
+        }
+    }
+    //-----------------------------------------------------------------------------
+    void slip_reset(uint8_t *buffer){
+        slip_buffer_header_t *slip_buffer_header = (slip_buffer_header_t *)buffer;
+        slip_buffer_header->len = 0;
+        slip_buffer_header->checksum = 0;
+        slip_buffer_header->ready = false;
+        slip_buffer_header->esc_flag = false;
+        slip_buffer_header->overflow = false;
+    }
+    //-----------------------------------------------------------------------------
+    uint8_t slip_is_ready(uint8_t *buffer){
+        slip_buffer_header_t *slip_buffer_header = (slip_buffer_header_t *)buffer;
+        if(slip_buffer_header->ready){
+            if(slip_buffer_header->checksum_enable == true){
+                uint32_t *checksum = (uint32_t*)(buffer + slip_buffer_header->len);
+                if(slip_buffer_header->checksum == *checksum){
+                    return(true);
+                }else{
+                    slip_reset(buffer);
+                    return(false);
+                }
+            }else{
+                return(true);
+            }
+        }
+        return(false);
+    }
+    //-----------------------------------------------------------------------------
+    uint32_t slip_get_size(uint8_t *buffer){
+        slip_buffer_header_t *slip_buffer_header = (slip_buffer_header_t *)buffer;
+        return(slip_buffer_header->len);
+    }
+    //-----------------------------------------------------------------------------
+    uint8_t *slip_get_buffer(uint8_t *buffer){
+        return(buffer + sizeof(slip_buffer_header_t));
+    }
+    
+#endif
--- src/serial_com.cpp
+++ src/serial_com.cpp
@@ -1,44 +1,41 @@
 //-----------------------------------------------------------------------------
 // File: serial_com.cpp
-// Last modified: 06/03/2025
+// Last modified: 27/03/2025
 //-----------------------------------------------------------------------------
 #include <Arduino.h>
 #include "serial_com.h"
-#include "slip_decoder.h"
 #include "command_handler.h"
 
+#define SLIP_IMPLEMENTATION
+#include "p_slip.h"
+
 static uint32_t checksum = 0;
+static uint8_t rx_slip_buffer[2028];
 
 //-----------------------------------------------------------------------------
 // Initialize the serial communication with the specified baud rate
 void serial_init(){
   Serial.begin(BAUDRATE);
+  slip_init(rx_slip_buffer, sizeof(rx_slip_buffer));
 }
 
 //-----------------------------------------------------------------------------
 // Handle serial communication tasks, including reading and processing commands
 void serial_task(){
   if(Serial.available()){
-    slip_push(Serial.read()); 
+    slip_push(rx_slip_buffer, Serial.read()); 
   }
-  if(slip_is_ready()){
-    size_t package_len = slip_package_size();
-    uint8_t *package = slip_get_package();
+  if(slip_is_ready(rx_slip_buffer)){
+    size_t package_len = slip_get_size(rx_slip_buffer);
+    uint8_t *package = slip_get_buffer(rx_slip_buffer);
     CMD_parse(package, package_len);
-    slip_reset();
-  }
-}
-//-----------------------------------------------------------------------------
-// Send a buffer using the SLIP protocol
-void serial_send_slip(uint8_t* buf, size_t len){
-  for(size_t i = 0; i < len; i++){
-    serial_send_slip(buf[i]);
+    slip_reset(rx_slip_buffer);
   }
 }
 //-----------------------------------------------------------------------------
 // Send a byte using the SLIP protocol
 void serial_send_slip(uint8_t data){
-  checksum += data;
+  checksum += data + 1;
   if(data == S_END){
     Serial.write(S_ESC);
     Serial.write(S_ESC_END);
@@ -49,6 +46,13 @@ void serial_send_slip(uint8_t data){
   }
   else{
     Serial.write(data);
+  }
+}
+//-----------------------------------------------------------------------------
+// Send a buffer using the SLIP protocol
+void serial_send_slip(uint8_t* buf, size_t len){
+  for(size_t i = 0; i < len; i++){
+    serial_send_slip(buf[i]);
   }
 }
 //-----------------------------------------------------------------------------
@@ -76,11 +80,8 @@ void serial_send_slip(String data){
 //-----------------------------------------------------------------------------
 // Send the end of the slip package
 void serial_end_slip(){
-  checksum = 0;
+  uint32_t final_checksum = checksum;
+  serial_send_slip(final_checksum);
   Serial.write(S_END);
-}
-//-----------------------------------------------------------------------------
-// Get total checksum
-uint32_t serial_get_checksum(){
-  return checksum;
+  checksum = 0;
 }
